0d272147c694c79ee2d05992acfe620f4e83ad65
Rafaela Bernardes Rabelo <165805309+RafaelaRabelo@users.noreply.github.com>
2025-06-13 15:51:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
 # Rota de login
 @app.route('/login')
 def login():
-    redirect_uri = url_for("authorize", _external=True)
+    redirect_uri = url_for("authorize", _external=True, _scheme='https')
     return google.authorize_redirect(redirect_uri)
 
 # Rota de callback do Google
