339eb89eec681ea092f78a1e7ce51c31fc8e4459
Rafaela Bernardes Rabelo <165805309+RafaelaRabelo@users.noreply.github.com>
2025-06-13 16:15:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ google = oauth.register(
     name='google',
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://oauth2.googleapis.com/token',
-    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v2/',
-    client_kwargs={'scope': 'openid email profile'},
-    authorize_params={'access_type': 'offline', 'prompt': 'consent'}
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile',
+        'prompt': 'consent',
+        'access_type': 'offline'
+    }
 )
 
 # Página principal
