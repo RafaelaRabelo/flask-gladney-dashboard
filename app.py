85a90ae53d2ae35b0c8da3f9ad17d5126d51b433
Rafaela Bernardes Rabelo <165805309+RafaelaRabelo@users.noreply.github.com>
2025-06-15 13:06:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import gspread
 import google.auth
 from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 
 load_dotenv()
 
